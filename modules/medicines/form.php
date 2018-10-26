1f2b3cd31b65981b012bd3fe8b73d8534dc0e81e
vanecarias <yarnellyblanco@gmail.com>
2018-10-26 11:36:21 -0400

    Se le cambio el titulo

--- modules/medicines/form.php
+++ modules/medicines/form.php
@@ -8,7 +8,7 @@ if ($_GET['form']=='add') { ?>
     </h1>
     <ol class="breadcrumb">
       <li><a href="?module=start"><i class="fa fa-home"></i> Inicio </a></li>
-      <li><a href="?module=medicines"> Medicamentos </a></li>
+      <li><a href="?module=medicines"> funcionarios </a></li>
       <li class="active"> Más </li>
     </ol>
   </section>
