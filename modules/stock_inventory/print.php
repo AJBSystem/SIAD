01a542490a4f7b47df705cea96d14ede45c19c5f
vanecarias <yarnellyblanco@gmail.com>
2018-10-30 16:12:31 -0400

    Se agregaron campos en el print reportes de funcionarios

--- modules/stock_inventory/print.php
+++ modules/stock_inventory/print.php
@@ -13,7 +13,7 @@ $hari_ini = date("d-m-Y");
 
 $no = 1;
 
-$query = mysqli_query($mysqli, "SELECT codigo,nombre,precio_compra,precio_venta,unidad,stock FROM medicamentos ORDER BY nombre ASC")
+$query = mysqli_query($mysqli, "SELECT cedula,credencial,p_nombre,s_nombre,p_apellido,s_apellido,fec_nac,edad,tel_cel,correo_e,precio_compra,precio_venta,unidad,stock FROM medicamentos ORDER BY nombre ASC")
                                 or die('Error '.mysqli_error($mysqli));
 $count  = mysqli_num_rows($query);
 ?>
@@ -25,7 +25,7 @@ $count  = mysqli_num_rows($query);
     </head>
     <body>
         <div id="title">
-           STOCK DE MEDICAMENTOS
+           Datos de Funcionarios 
         </div>
         
         <hr><br>
@@ -35,10 +35,17 @@ $count  = mysqli_num_rows($query);
                 <thead style="background:#e8ecee">
                     <tr class="tr-title">
                         <th height="20" align="center" valign="middle"><small>NO.</small></th>
-                        <th height="20" align="center" valign="middle"><small>CODIGO</small></th>
-                        <th height="20" align="center" valign="middle"><small>MEDICAMENTO</small></th>
-                        <th height="20" align="center" valign="middle"><small>PRECIO DE COMPRA</small></th>
-                        <th height="20" align="center" valign="middle"><small>PRECIO DE VENTA</small></th>
+                        <th height="20" align="center" valign="middle"><small>Cedula</small></th>
+                        <th height="20" align="center" valign="middle"><small>Credencial</small></th>
+                        <th height="20" align="center" valign="middle"><small>Primer Nombre</small></th>
+                        <th height="20" align="center" valign="middle"><small>Segundo Nombre</small></th>
+                        <th height="20" align="center" valign="middle"><small>Primer Apellido</small></th>
+                        <th height="20" align="center" valign="middle"><small>Segundo Apellido</small></th>
+                        <th height="20" align="center" valign="middle"><small>Fecha de Nacimiento</small></th>
+                        <th height="20" align="center" valign="middle"><small>Edad</small></th>
+                        
+                        <th height="20" align="center" valign="middle"><small>Telefono Celular</small></th>
+                        <th height="20" align="center" valign="middle"><small>Correo Electronico</small></th>
                         <th height="20" align="center" valign="middle"><small>STOCK</small></th>
                         <th height="20" align="center" valign="middle"><small>UNIDAD</small></th>
                     </tr>
@@ -52,8 +59,16 @@ $count  = mysqli_num_rows($query);
           
             echo "  <tr>
                         <td width='40' height='13' align='center' valign='middle'>$no</td>
-                        <td width='80' height='13' align='center' valign='middle'>$data[codigo]</td>
-                        <td style='padding-left:5px;' width='180' height='13' valign='middle'>$data[nombre]</td>
+                        <td width='80' height='13' align='center' valign='middle'>$data[cedula]</td>
+                        <td width='80' height='13' align='center' valign='middle'>$data[credencial]</td>
+                        <td style='padding-left:5px;' width='140' height='13' valign='middle'>$data[p_nombre]</td>
+                        <td style='padding-left:5px;' width='140' height='13' valign='middle'>$data[s_nombre]</td>
+                        <td style='padding-left:5px;' width='140' height='13' valign='middle'>$data[p_apellido]</td>
+                        <td style='padding-left:5px;' width='140' height='13' valign='middle'>$data[s_apellido]</td>
+                        <td style='padding-left:5px;' width='100' height='13' valign='middle'>$data[fec_nac]</td>
+                        <td style='padding-left:5px;' width='80' height='13' valign='middle'>$data[edad]</td>
+                        <td style='padding-left:5px;' width='100' height='13' valign='middle'>$data[tel_cel]</td>
+                        <td style='padding-left:5px;' width='100' height='13' valign='middle'>$data[correo_e]</td>
                         <td style='padding-right:10px;' width='80' height='13' align='right' valign='middle'>$. $precio_compra</td>
                         <td style='padding-right:10px;' width='80' height='13' align='right' valign='middle'>$. $precio_venta</td>
                         <td style='padding-right:10px;' width='80' height='13' align='right' valign='middle'>$data[stock]</td>
@@ -78,7 +93,7 @@ $content = '<page style="font-family: freeserif">'.($content).'</page>';
 require_once('../../assets/plugins/html2pdf_v4.03/html2pdf.class.php');
 try
 {
-    $html2pdf = new HTML2PDF('P','F4','en', false, 'ISO-8859-15',array(10, 10, 10, 10));
+    $html2pdf = new HTML2PDF('L','F4','en', false, 'ISO-8859-15',array(10, 10, 10, 10));
     $html2pdf->setDefaultFont('Arial');
     $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
     $html2pdf->Output($filename);
