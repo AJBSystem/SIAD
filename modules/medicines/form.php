47ac4a5f2c781a7e0fb7b94bcf843a70e5209290
C4R4C4S <darlynmartinezb@gmail.com>
2018-11-08 13:32:45 -0430

    Se agregaron los campos del modulo de informacion medica

--- modules/medicines/form.php
+++ modules/medicines/form.php
@@ -290,10 +290,14 @@ if ($_GET['form']=='add') { ?>
             </div>
 
             <div class="col-md-2">
-             <label for="exampleInputEmail1">Discapacidad</label>
-              <input type="text" class="form-control" name="discapacidad" autocomplete="off" required> 
+             <label for="exampleInputEmail1">Alergias</label>
+              <input type="text" class="form-control" name="alergias" autocomplete="off" required> 
             </div>
 
+            <div class="col-md-2">
+             <label for="exampleInputEmail1">Notas</label>
+              <input type="text" class="form-control" name="notas" autocomplete="off" required> 
+            </div>
 
 </div>
 
@@ -334,7 +338,7 @@ elseif ($_GET['form']=='edit') {
   if (isset($_GET['id'])) {
 
 
-      $query = mysqli_query($mysqli, "SELECT codigo,cedula,credencial,rif,p_nombre,s_nombre,p_apellido,s_apellido,fec_nac,edad,lugar_nac,genero,tel_cel,tel_hab,tel_emerg,estado,estado_c,grado_inst,correo_e,municipio,ciudad,parroquia,avenida,calle,casa,codigo_cp,serial_cp,tipo_s,medicamen,discapacidad FROM medicamentos WHERE codigo='$_GET[id]'") 
+      $query = mysqli_query($mysqli, "SELECT codigo,cedula,credencial,rif,p_nombre,s_nombre,p_apellido,s_apellido,fec_nac,edad,lugar_nac,genero,tel_cel,tel_hab,tel_emerg,estado,estado_c,grado_inst,correo_e,municipio,ciudad,parroquia,avenida,calle,casa,codigo_cp,serial_cp,tipo_s,medicamen,discapacidad,alergias,notas FROM medicamentos WHERE codigo='$_GET[id]'") 
 
                                       or die('error: '.mysqli_error($mysqli));
       $data  = mysqli_fetch_assoc($query);
