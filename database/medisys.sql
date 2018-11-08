0b5598ca37af516f432ee9784cdcf224ac92cafc
C4R4C4S <darlynmartinezb@gmail.com>
2018-11-08 12:06:28 -0430

    Se actualizo la base de datos

--- database/medisys.sql
+++ database/medisys.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-10-2018 a las 21:09:17
+-- Tiempo de generación: 08-11-2018 a las 17:35:23
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 5.6.38
 
@@ -92,17 +92,23 @@ CREATE TABLE `medicamentos` (
   `lugar_nac` varchar(50) NOT NULL,
   `correo_e` varchar(50) NOT NULL,
   `grado_inst` varchar(50) NOT NULL,
-  `correo_i` varchar(50) NOT NULL
+  `correo_i` varchar(50) NOT NULL,
+  `tipo_s` varchar(50) NOT NULL,
+  `medicamen` varchar(100) NOT NULL,
+  `discapacidad` varchar(100) NOT NULL,
+  `alergias` varchar(100) NOT NULL,
+  `nota` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `medicamentos`
 --
 
-INSERT INTO `medicamentos` (`codigo`, `nombre`, `precio_compra`, `precio_venta`, `unidad`, `stock`, `created_user`, `created_date`, `updated_user`, `updated_date`, `p_nombre`, `cedula`, `credencial`, `p_apellido`, `fec_nac`, `edad`, `s_nombre`, `s_apellido`, `rif`, `genero`, `tel_cel`, `tel_hab`, `tel_emerg`, `municipio`, `calle`, `casa`, `parroquia`, `estado`, `ciudad`, `avenida`, `codigo_cp`, `serial_cp`, `estado_c`, `lugar_nac`, `correo_e`, `grado_inst`, `correo_i`) VALUES
-('141', '', 0, 0, '', 10, 1, '2018-10-29 14:30:10', 1, '2018-10-29 16:09:35', 'darly', 26435880, 10254, 'masrtinez', '29/10/1998', 20, 'naiduth', 'barreto', 2965956, 0, 2147483647, 2123174277, 2147483647, 'libertador', 'callejon', '88', 'antimano', 'distrito capital', 'caracas', 'primer carapot', 29659459, 4949, '', '', '', '', ''),
-('B000142', '', 0, 0, '', -10, 1, '2018-10-29 14:51:43', 1, '2018-10-29 15:34:47', 'fdfn', 15841949, 49049, 'fjgf', '19/140', 11, 'gg', 'tjfj', 4949, 0, 2147483647, 29292, 929, 'sfdsdf', 'fds', '22', 'fsaf', 'fsdf', 'fsfd', 'dfs', 29529, 295, '', '', '', '', ''),
-('B000143', '', 0, 0, '', 0, 1, '2018-10-29 16:09:15', 1, '2018-10-30 16:24:48', '523523', 582, 85232, '27', '5272', 3274, '523532', '2745', 5237, 0, 5, 581281, 587, '75', '587', '528', '57', 'Amazonas', '875', '58', 0, 8750, 'Soltero/a', 'Amazonas', 'darly@gmail.com', 'Primaria', '');
+INSERT INTO `medicamentos` (`codigo`, `nombre`, `precio_compra`, `precio_venta`, `unidad`, `stock`, `created_user`, `created_date`, `updated_user`, `updated_date`, `p_nombre`, `cedula`, `credencial`, `p_apellido`, `fec_nac`, `edad`, `s_nombre`, `s_apellido`, `rif`, `genero`, `tel_cel`, `tel_hab`, `tel_emerg`, `municipio`, `calle`, `casa`, `parroquia`, `estado`, `ciudad`, `avenida`, `codigo_cp`, `serial_cp`, `estado_c`, `lugar_nac`, `correo_e`, `grado_inst`, `correo_i`, `tipo_s`, `medicamen`, `discapacidad`, `alergias`, `nota`) VALUES
+('B000001', '', 0, 0, '', 0, 1, '2018-11-08 15:01:11', 1, '2018-11-08 15:01:11', 'Dalry', 26435880, 26435, 'Martinez', '29/10/1998', 20, 'Naiduth', 'Barreto', 264358801, 0, 2147483647, 2124170277, 2147483647, 'Libertador', 'callejon pinto salina', '88', 'Antimano', 'Distrito Capital', 'Caracas', 'Primera entrada de carapita', 295959, 2595959, 'Soltero/a', 'Amazonas', 'darlynmartinezb@gmail.com', 'Primaria', '', '', '', '', '', ''),
+('B000002', '', 0, 0, '', 0, 1, '2018-11-08 15:19:17', 1, '2018-11-08 15:19:17', '5', 659, 59, '498', '45', 9845, '985', '98', 59, 0, 489, 8, 89, 'jhk', '8', '98', '78', '4789', '889', 'yjt', 8, 798, 'Soltero/a', 'Amazonas', '89', 'Primaria', '', 'bd', '', '', '', ''),
+('B000003', '', 0, 0, '', 0, 1, '2018-11-08 15:22:58', 1, '2018-11-08 15:22:58', 'fsdfsdfsd', 0, 0, 'sdfdsfsdf', 'sdfsd', 0, 'sfsd', 'fsdf', 0, 0, 0, 0, 0, 'fsdfds', 'fds', 'fdsf', 'sdfsd', 'f', 'sdfsd', 'dsff', 0, 0, 'Soltero/a', 'Amazonas', 'sdf', 'Primaria', '', 'nb', 'ffsdfsd', '', '', ''),
+('B000004', '', 0, 0, '', 0, 1, '2018-11-08 15:26:25', 1, '2018-11-08 15:26:25', '165', 56, 156, '1', '56', 1, '156', '651', 165, 0, 561, 51, 561, '156', '1', '561', '51', '56', '1', '5', 561, 61, 'Soltero/a', 'Amazonas', '61', 'Primaria', '', 'bd', 'pastillas', 'silla de ruedas', '', '');
 
 -- --------------------------------------------------------
 
@@ -119,14 +125,6 @@ CREATE TABLE `transaccion_medicamentos` (
   `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `tipo_transaccion` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `transaccion_medicamentos`
---
-
-INSERT INTO `transaccion_medicamentos` (`codigo_transaccion`, `fecha`, `codigo`, `numero`, `created_user`, `created_date`, `tipo_transaccion`) VALUES
-('TM-2018-0000001', '2018-10-29', 'B000142', 10, 1, '2018-10-29 15:34:46', 'Salida'),
-('TM-2018-0000002', '2018-10-29', '141', 10, 1, '2018-10-29 16:09:35', 'Entrada');
 
 -- --------------------------------------------------------
 
@@ -154,8 +152,9 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id_user`, `username`, `name_user`, `password`, `correo_i`, `telefono`, `foto`, `permisos_acceso`, `status`, `created_at`, `updated_at`) VALUES
 (1, 'admin', 'Administrador', '21232f297a57a5a743894a0e4a801fc3', 'darly@sist.com', '04140272330', 'Desert.jpg', 'Super Admin', 'activo', '2017-04-01 08:15:15', '2018-10-30 15:11:13'),
-(2, 'juan', 'juan', 'a94652aa97c7211ba8954dd15a3cf838', 'juab@juan.com', '12000', NULL, 'Almacen', 'bloqueado', '2017-07-25 22:34:03', '2018-10-30 14:09:16'),
-(3, 'Darly', 'Darly Martinez', 'f1fd75612dfa7dc86f07398fb9145fbf', NULL, NULL, NULL, 'Almacen', 'activo', '2018-10-30 13:42:24', '2018-10-30 13:42:24');
+(2, 'juan', 'juan', 'a94652aa97c7211ba8954dd15a3cf838', 'juab@juan.com', '12000', NULL, 'Almacen', 'activo', '2017-07-25 22:34:03', '2018-11-01 13:58:46'),
+(3, 'Darly', 'Darly Martinez', 'f1fd75612dfa7dc86f07398fb9145fbf', '', '', NULL, 'Almacen', 'activo', '2018-10-30 13:42:24', '2018-11-01 13:58:12'),
+(4, 'vanesa', 'vanesa', 'f1fd75612dfa7dc86f07398fb9145fbf', NULL, NULL, NULL, 'Gerente', 'activo', '2018-11-01 13:59:01', '2018-11-01 13:59:01');
 
 --
 -- Índices para tablas volcadas
@@ -192,7 +191,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
