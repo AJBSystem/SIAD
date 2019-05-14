e7a251e9905ed5b341504191698dba932f3adac6
C4R4C4S <darlynmartinezb@gmail.com>
2019-05-14 10:00:54 -0400

    Se agrego el modal en editar funcional

--- modules/medicines/form.php
+++ modules/medicines/form.php
@@ -956,7 +956,36 @@ elseif ($_GET['form']=='edit') {
                                       <input type="text" class="form-control" name="tel_emerg" autocomplete="off" value="<?php echo $data['tel_emerg']; ?>" required>
                                   </div>
 
-                                    <!-- <div class="col-md-2">
+
+                                                  <div class="col-md-2">
+                                                    <label for="exampleInputEmail1">Codigo del CP</label>
+                                                      <input type="text" class="form-control" name="codigo_cp" autocomplete="off" value="<?php echo $data['codigo_cp']; ?>">
+                                                  </div>
+
+                                                    <div class="col-md-2">
+                                                      <label for="exampleInputEmail1">Serial del CP</label>
+                                                        <input type="text" class="form-control" name="serial_cp" autocomplete="off" value="<?php echo $data['serial_cp']; ?>">
+                                                    </div>
+
+<!-- Final del modal de direccion -->
+
+                                                    <div class="contenedor-modal">
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#miModal">Direción</button>
+</div>
+
+<div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h4 class="modal-title" id="myModalLabel">Agregar Dirección</h4>
+      </div>
+     
+  <table class='table table-bordered'>
+
+<div class="col-md-4">
                                       <label for="exampleInputEmail1">Estado</label>
                                         <select type="text" class="form-control" name="estado" autocomplete="off" value="<?php echo $data['estado']; ?>" required>
                                           <option>Amazonas</option>
@@ -986,50 +1015,45 @@ elseif ($_GET['form']=='edit') {
                                         </select>
                                     </div> 
 
-                                      <div class="col-md-2">
+                                      <div class="col-md-4">
                                         <label for="exampleInputEmail1">Municipio</label>
                                           <input type="text" class="form-control" name="municipio" autocomplete="off" value="<?php echo $data['municipio']; ?>" required>
                                       </div> 
 
-                                        <div class="col-md-2">
+                                        <div class="col-md-4">
                                           <label for="exampleInputEmail1">Ciudad</label>
                                             <input type="text" class="form-control" name="ciudad" autocomplete="off" value="<?php echo $data['ciudad']; ?>" required>
                                         </div>
 
-                                          <div class="col-md-2">
+                                          <div class="col-md-4">
                                             <label for="exampleInputEmail1">Parroquia</label>
                                               <input type="text" class="form-control" name="parroquia" autocomplete="off" value="<?php echo $data['parroquia']; ?>" required>
                                           </div>
 
-                                            <div class="col-md-2">
+                                            <div class="col-md-4">
                                               <label for="exampleInputEmail1">Avenida</label>
                                                 <input type="text" class="form-control" name="avenida" autocomplete="off" value="<?php echo $data['avenida']; ?>" required>
                                             </div>
 
-                                              <div class="col-md-2">
+                                              <div class="col-md-4">
                                                 <label for="exampleInputEmail1">Calle</label>
                                                   <input type="text" class="form-control" name="calle" autocomplete="off" value="<?php echo $data['calle']; ?>" required>
                                               </div>
 
-                                                <div class="col-md-2">
+
+            <div class="col-md-4">
                                                   <label for="exampleInputEmail1">Casa N°</label>
                                                     <input type="text" class="form-control" name="casa" autocomplete="off" value="<?php echo $data['casa']; ?>" required>
-                                                </div> -->
+                                                </div>
 
-                                                  <div class="col-md-2">
-                                                    <label for="exampleInputEmail1">Codigo del CP</label>
-                                                      <input type="text" class="form-control" name="codigo_cp" autocomplete="off" value="<?php echo $data['codigo_cp']; ?>">
-                                                  </div>
+  </table>
+  
+    </div>
+  </div>
+</div>
 
-                                                    <div class="col-md-2">
-                                                      <label for="exampleInputEmail1">Serial del CP</label>
-                                                        <input type="text" class="form-control" name="serial_cp" autocomplete="off" value="<?php echo $data['serial_cp']; ?>">
-                                                    </div>
-
-                                                      <div class="col-md-5">
-                                                        <label for="exampleInputEmail1">Dirección</label>
-                                                          <textarea rows="2" class="form-control" name="direccion" autocomplete="off" value="<?php echo $data['direccion']; ?>"></textarea>
-                                                      </div>
+<!-- Final del modal de direccion -->
+                                           
 
 </div>
 </div>
