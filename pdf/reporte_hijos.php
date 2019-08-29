acd83c2430aa71d979a9dc2cb7925a17258c1ab9
Darly <darlynmartinezb@gmail.com>
2019-08-29 09:53:55 -0400

    Se agrego el reporte de hijos

--- pdf/reporte_hijos.php
+++ pdf/reporte_hijos.php
@@ -1,7 +1,7 @@
 <?php
   include('plantilla_hijos.php');
   require('conexion.php');
-  $query = "SELECT cedulaf,p_nombref,s_nombref,p_apellidof,s_apellidof,fec_nacf,edadf,generof,estudiante,funcionario,inf_medf,p_nombre,p_apellido
+  $query = "SELECT cedulaf,cedula,p_nombref,s_nombref,p_apellidof,s_apellidof,fec_nacf,edadf,generof,estudiante,funcionario,inf_medf,p_nombre,p_apellido
             FROM medicamentos 
             ORDER BY cedulaf";
           
@@ -15,10 +15,10 @@
   $pdf->SetX(8);
   
    // <!-- Final de titulos -->
-  $pdf->Cell(40,7,utf8_decode('Cedula del Funcionario'),1,0,'C',1);
-  $pdf->Cell(68,7,utf8_decode('Nombre del funcionario'),1,0,'C',1);
-  $pdf->Cell(72,7,utf8_decode('Apellido del funcionario'),1,0,'C',1);
-  $pdf->Cell(40,7,utf8_decode('Cedula del hijo'),1,0,'C',1);
+  $pdf->Cell(53,7,utf8_decode('Cedula del Funcionario'),1,0,'C',1);
+  $pdf->Cell(54,7,utf8_decode('Nombre del funcionario'),1,0,'C',1);
+  $pdf->Cell(54,7,utf8_decode('Apellido del funcionario'),1,0,'C',1);
+  $pdf->Cell(35,7,utf8_decode('Cedula del hijo'),1,0,'C',1);
   //Por ahora se llamara nombre del hijo
   $pdf->Cell(42,7,utf8_decode('Primer Nombre'),1,0,'C',1);
   $pdf->Cell(42,7,utf8_decode('Segundo Nombre'),1,0,'C',1);
@@ -29,15 +29,16 @@
   $pdf->Cell(30,7,utf8_decode('Género'),1,0,'C',1);
   $pdf->Cell(27,7,utf8_decode('Estudiante'),1,0,'C',1);
   $pdf->Cell(27,7,utf8_decode('Funcionario'),1,0,'C',1);
-  $pdf->Cell(50,7,utf8_decode('Información Médica'),1,1,'C',1);
+  $pdf->Cell(60,7,utf8_decode('Información Médica'),1,1,'C',1);
   // <!-- Final de titulos -->
   
   while ( $row = $resultado->fetch_assoc() )
   {
    $pdf->SetX(8);
-   $pdf->Cell(68,7,utf8_decode($row['p_nombre']),1,0,'C',0);
-   $pdf->Cell(72,7,utf8_decode($row['p_apellido']),1,0,'C',0);
-   $pdf->Cell(40,7,utf8_decode($row['cedulaf']),1,0,'C',0);
+   $pdf->Cell(53,7,utf8_decode($row['cedula']),1,0,'C',0);
+   $pdf->Cell(54,7,utf8_decode($row['p_nombre']),1,0,'C',0);
+   $pdf->Cell(54,7,utf8_decode($row['p_apellido']),1,0,'C',0);
+   $pdf->Cell(35,7,utf8_decode($row['cedulaf']),1,0,'C',0);
    $pdf->Cell(42,7,utf8_decode($row['p_nombref']),1,0,'C',0); 
    $pdf->Cell(42,7,utf8_decode($row['s_nombref']),1,0,'C',0); 
    $pdf->Cell(42,7,utf8_decode($row['p_apellidof']),1,0,'C',0); 
@@ -47,7 +48,7 @@
    $pdf->Cell(30,7,utf8_decode($row['generof']),1,0,'C',0); 
    $pdf->Cell(27,7,utf8_decode($row['estudiante']),1,0,'C',0); 
    $pdf->Cell(27,7,utf8_decode($row['funcionario']),1,0,'C',0);     
-   $pdf->Cell(50,7,utf8_decode($row['inf_medf']),1,1,'C',0);  
+   $pdf->Cell(60,7,utf8_decode($row['inf_medf']),1,1,'C',0);  
   }
   $pdf->Output();
 ?>
